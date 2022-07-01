e2243933a2919633e7bd4137879d2d6f1e547d08
morrisonibim'cleargit config --global user.name morrisonibimgit config --global user.name "morrisonibim <morrisonibim@gmail.com>
2022-07-01 14:49:47 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
- * main - main block
- * Description: Print all possible combinations of two two-digit numbers.
- * Numbers should range from 0 to 99.
- * The two numbers should be separated by a space.
- * All numbers should be printed with two digits. 1 should be printed as 01.
- * Combination of numbers must be separated by a comma followed by a space.
- * Combinations of numbers should be printed in ascending order.
- * `00 01` and `01 00` are considered as the same combination.
- * You can only use `putchar` to print to console.
- * You can only use `putchar` up to 8 times.
- * You are not allowed to use any variable of type `char`.
- * Return: 0
- */
+* main - main block
+* Description: print all possible combinations oftwo two-digit numbers.
+* The numbers should range from 0 to 99
+* The two numbers should be separated by a space
+* All numbers should be printed with two digits. 1 should be printed as 01
+* The combination of numbers must be separated by comma, followed by a space
+* The combinations of numbers should be printed in ascending order
+* 00 01 and 01 00 are considered as the same combination of the numbers 0 ad 1
+* You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+* You can only use putchar eight times maximum in your cod
+* You are not allowed to use any variable of type char
+*Return: 0
+*/
 int main(void)
 {
 	int i, j;
@@ -46,6 +46,6 @@ int main(void)
 	}
 	putchar(10);
 
-	retun (10);
+	return (0);
 }
 
