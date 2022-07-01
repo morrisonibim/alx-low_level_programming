bd40463ed11d4880f8cfabb8496a4eeba3376ae3
morrisonibim'cleargit config --global user.name morrisonibimgit config --global user.name "morrisonibim <morrisonibim@gmail.com>
2022-07-01 13:58:55 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- *main - main block
- *Description: Print all possible combinations of two two-digit numbers.
- *Numbers should range from 0 to 99.
- *The two numbers should be separated by a space.
- *All numbers should be printed with two digits. 1 should be printed as 01.
- *Combination of numbers must be separated by a comma followed by a space.
- *Combinations of numbers should be printed in ascending order.
- *`00 01` and `01 00` are considered as the same combination.
- *You can only use `putchar` to print to console.
- *You can only use `putchar` up to 8 times
- *You are not allowed to use any variable of type `char`.
- *Return: 0
+ * main - main block
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01.
+ * Combination of numbers must be separated by a comma followed by a space.
+ * Combinations of numbers should be printed in ascending order.
+ * `00 01` and `01 00` are considered as the same combination.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 8 times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
  */
 int main(void)
 {
@@ -22,7 +22,7 @@ int main(void)
 	{
 		a = i / 10; /* doubles fnum */
 		b = i % 10; /* singles fnum */
-		
+
 		for (j = 0; j < 100; j++)
 		{
 			c = j / 10; /* doubles snum */
@@ -30,7 +30,6 @@ int main(void)
 
 			if (a < c || (a == c && b < d))
 			{
-
 				putchar(a + '0');
 				putchar(b + '0');
 				putchar(32);
@@ -47,7 +46,6 @@ int main(void)
 	}
 	putchar(10);
 
-	return (0);
+	retun (10);
 }
-
 
