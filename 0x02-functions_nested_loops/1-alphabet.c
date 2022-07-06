29b211cf348f0629818699e3ccf3cb267d646d10
morrisonibim'cleargit config --global user.name morrisonibimgit config --global user.name "morrisonibim <morrisonibim@gmail.com>
2022-07-06 00:38:39 -0700

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,6 +2,8 @@ include "main.h"
 
 /**
  * print_alphabet -> prints the lowercase alphabets
+ * Description: It prints the alphabets in lowercase followed by a new line
+ * Return: Always 0.
  */
 
 void print_alphabet(void)
