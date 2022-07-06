d2f6766a2d1d011956aa5a9cb5925ac1bdd8ba0e
morrisonibim'cleargit config --global user.name morrisonibimgit config --global user.name "morrisonibim <morrisonibim@gmail.com>
2022-07-06 04:30:13 -0700

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,33 +1,36 @@
 #include "main.h"
 
 /**
- * times_table - 9 times table
- *
- * Return: void
+ * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
 {
-	int j, k, opp;
+  int i, j, k;
 
-	for (j = 0; j <= 9; j++)
+  for (i = 0; i < 10; i++)
+    {
+      for (j = 0; j < 10; j++)
 	{
-		_putchar(48);
-		for (k = 1; k <= 9; k++)
-		{
-			opp = j * k;
-			_putchar(44);
-			_putchar(32);
-			if (opp <= 9)
-			{
-				_putchar(32);
-				_putchar(opp + 48);
-			}
-			else
-			{
-				_putchar((opp / 10) + 48);
-				_putchar((opp % 10) + 48);
-			}
-		}
-		_putchar('n\');
+	  k = j * i;
+	  if (j == 0)
+	    {
+	      _putchar(k + '0');
+	    }
+
+	  if (k < 10 && j != 0)
+	    {
+	      _putchar(',');
+	      _putchar(' ');
+	      _putchar(' ');
+	      _putchar(k + '0');
+	    } else if (k >= 10)
+	    {
+	      _putchar(',');
+	      _putchar(' ');
+	      _putchar((k / 10) + '0');
+	      _putchar((k % 10) + '0');
+	    }
 	}
+      _putchar('\n');
+    }
 }
