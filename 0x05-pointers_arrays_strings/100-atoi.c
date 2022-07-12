a04a3e313e0f2c950daf8e2c6869f416b4c01ce1
morrisonibim'cleargit config --global user.name morrisonibimgit config --global user.name "morrisonibim <morrisonibim@gmail.com>
2022-07-12 14:50:04 -0700

    done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,30 +7,29 @@
  */
 int _atoi(char *s)
 {
-  unsigned int count = 0, size = 0, oi = 0, pt = 1, m = 1, i;
+	unsigned int count = 0, size = 0, oi = 0, pt = 1, m = 1, i;
 
-  while (*(s + count) != '\0')
-    {
-      if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-	break;
-
-      if (*(s + count) == '-')
-	pt *= -1;
-
-      if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+	while (*(s + count) != '\0')
 	{
-	  if (size > 0)
-	    m *= 10;
-	  size++;
-	}
-      count++;
-    }
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
 
-  for (i = count - size; i < count; i++)
-    {
-      oi = oi + ((*(s + i) - 48) * m);
-      m /= 10;
-    }
-  return (oi * pt);
+		if (*(s + count) == '-')
+			pt *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pt);
 }
-~
