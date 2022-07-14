4f1d64829c4dce8716760d30c7d4d0828baaac01
morrisonibim'cleargit config --global user.name morrisonibimgit config --global user.name "morrisonibim <morrisonibim@gmail.com>
2022-07-14 09:06:03 -0700

    done

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,21 +7,21 @@
  */
 char *rot13(char *s)
 {
-  int count = 0, i;
-  char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKKM";
+	int count = 0, i;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKKM";
 
-  while (*(s + count) != '\0')
-    {
-      for (i = 0; i < 52; i++)
+	while (*(s + count) != '\0')
 	{
-	  if (*(s + count) == alpha[i])
-	    {
-	      *(s + count) = rot13[i];
-	      break;
-	    }
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + count) == alpha[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
 	}
-      count++;
-    }
-  return (s);
+	return (s);
 }
