5be6d72589bb7035ae00445ae6803165483a6462
morrisonibim'cleargit config --global user.name morrisonibimgit config --global user.name "morrisonibim <morrisonibim@gmail.com>
2022-08-04 00:16:10 -0700

    done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array
+ * array_iterator - executes a function given as a parameter
  * @array: input integer array
  * @size: size of the array
  * @action: pointer to the function
@@ -9,10 +9,14 @@
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
-{
-	unsigned int i;
 
-	if (array && action)
-		for (i = 0; i < size; i++)
-			action(array[i]);
+	{
+if (array == NULL || action == NULL)
+return;
+
+while (size-- > 0)
+{
+action(*array);
+array++;
+}
 }
